74f9c020d8939ba8849c26bc535b25f8fb95e59b
00sukhjeet00 <sukhjeet20002@gmail.com>
2021-08-28 10:59:19 +0530

    new commit

--- Route/teacherRoute.js
+++ Route/teacherRoute.js
@@ -1,7 +1,24 @@
 const { Router } = require('express')()
 const jwt=require('jsonwebtoken')
 const teacherRouter = Router()
-teacherRouter.post('/createclassroom', (req, res) => {
+const createClass=require('../Modal/createClassModal')
+teacherRouter.post('/create/classroom', (req, res) => {
     const { token, subjectName, subjectCode, time,code } = req.body
+    jwt.verify(token, "NULL", (err,decoded) => {
+        if (err) throw err
+        if (decoded.isTeacher)
+        {
+            const newClass = new createClass({ subjectName, subjectCode, time, code })
+            newClass.save(err => {
+                if (err) throw err
+                res.json({msg:"Successfully Created Class Room"})
+            })
+        }
+        else {
+            res.status(401).json({msg:"Invalid Token"})
+        }
+    })
+})
+teacherRouter.post('/create/test', (req, res) => {
     
 })
