63ebe8143519d4d9e57c022214c727f2b66017ff
00sukhjeet00 <sukhjeet20002@gmail.com>
2021-08-28 11:33:11 +0530

    new commit

--- Route/teacherRoute.js
+++ Route/teacherRoute.js
@@ -19,6 +19,21 @@ teacherRouter.post('/create/classroom', (req, res) => {
         }
     })
 })
+teacherRouter.get('/classes', (req, res) => {
+    jwt.verify(token, "NULL", (err, decoded) => {
+        if (err) throw err
+        if (decoded.isTeacher)
+        {
+            createClass.findOne({ teacher: decoded.userId }, (err, docs) => {
+                if(err) throw err
+                res.json({classes:docs})
+            })   
+        }
+        else {
+            res.status(401).json({msg:"Invalid Token"})
+        }
+    })
+})
 teacherRouter.post('/create/test', (req, res) => {
     
 })
