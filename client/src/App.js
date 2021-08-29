bb0fffd4af9680327fa03090c200ae1fa306b864
00sukhjeet00 <sukhjeet20002@gmail.com>
2021-08-29 15:08:04 +0530

    new commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,37 +7,25 @@ import TeacherLogin from "./Screen/TeacherLogin";
 import StudentRegister from "./Screen/StudentRegister";
 import TeacherRegister from "./Screen/TeacherRegister";
 import LoginScreen from "./Screen/LoginScreen";
-
-
-// import Login from "./components/Login";
-
+import TeacherDashboardClass from './Screen/TeacherDashboardClass';
+import TeacherDashboardTest from './Screen/TeacherDashboardTest';
+import TeacherDashboardAssigment from './TeacherDashboardAssigment';
 function App() {
-  
   return (
     <div>
-  
       <BrowserRouter>
         <Nav/>
-        
-        {/* <Route  component={Login} path="/login"/> */}
         <Route exact component={HomeScreen} path="/" />
-        
-
-
-
-
-
+        <Route component={LoginScreen} path="/login" />
         <Route component={StudentLogin} path="/student/login" />
         <Route component={TeacherLogin} path="/teacher/login" />
         <Route component={StudentRegister} path="/student/register" />
         <Route component={TeacherRegister} path="/teacher/register" />
-        <Route component={LoginScreen} path="/login"/>
-        {/* register */}
-          
+        <Route component={TeacherDashboardTest} path="/dashboard/test" />
+        <Route component={TeacherDashboardAssigment} path="/dashboard/assigment"/>
+        <Route component={TeacherDashboardClass} path="/dashboard/class"/>
       </BrowserRouter>
-   
     </div>
   );
 }
-
 export default App;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -8,30 +8,23 @@ export default function Login(props) {
                 <form>
                     <div className="inputBox">
                         <input type="text" name="" required=""></input>
-                        <label for="">Username</label>
+                        <label for="">Name</label>
                     </div>
                      <div className="inputBox">
                         <input type="password" name="" required=""></input>
                         <label for="">Password</label>
                     </div>
+                    <div className="inputBox">
+                        <input type="email" name="" required=""></input>
+                        <label for="">Email</label>
+                    </div>
+                    <div className="inputBox">
+                        <input type="text" name="" required=""></input>
+                        <label for="">Institute Name</label>
+                    </div>
                     <input type="submit" name="" value="Register"></input>
-
                 </form>
             </div>
-           
-                         {/* <h2>Login</h2>
-                            <form action="">
-                                <div class="inputBox">
-                                    <input type="text" name="" required="">
-                                    <label for="">Username</label>
-                                </div>
-                                <div class="inputBox">
-                                    <input type="password" name="" required="">
-                                    <label for="">Password</label>
-                                </div>
-                                <input type="submit" name="" value="Submit">
-                            </form>
-              */}
       </div>
        
     )
