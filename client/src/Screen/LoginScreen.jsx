05b80c1a8b356d9600f90a47e4cc4248e7afdcbe
lokesh21012002 <gawandelokesh123@gmail.com>
2021-08-29 08:04:30 -0700

    Lokesh

--- client/src/Screen/LoginScreen.jsx
+++ client/src/Screen/LoginScreen.jsx
@@ -12,7 +12,7 @@ export default function LoginScreen() {
       </Link>
       <Link to="/teacher/login" style={{"textDecoration":"none","color":"black","backgroundColor"
     :"white","marginTop":"4rem"}}>
-        <h2>As Student</h2>
+        <h2>As Teacher</h2>
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.JswZOq7vdMSDgmbwytZonQHaHa%26pid%3DApi&f=1" alt="#"/>
       </Link>
     </div>
